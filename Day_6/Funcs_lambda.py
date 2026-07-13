2c4c9f581542c779c55862ba23e34b5d617c0d8c
mayurrishii <mayur.rishi@prodapt.com>
2026-07-13 15:02:19 +0530

    time difference

--- Day_6/Funcs_lambda.py
+++ Day_6/Funcs_lambda.py
@@ -3,12 +3,42 @@
 
 # print(square(5)) 
 
-def is_palindrome(string):
-    string = string.lower().replace(" ", "")
-    return string == string[::-1]
+# def is_palindrome(string):
+#     string = string.lower().replace(" ", "")
+#     return string == string[::-1]
 
-word = input("Enter a word to check if it's a palindrome: ")
-if is_palindrome(word): 
-    print(f"{word} is a palindrome.")
-else:
-    print(f"{word} is not a palindrome.")
+# word = input("Enter a word to check if it's a palindrome: ")
+# if is_palindrome(word): 
+#     print(f"{word} is a palindrome.")
+# else:
+#     print(f"{word} is not a palindrome.")
+
+
+
+# add = lambda a,b : a + b
+# sub = lambda a,b : a - b
+
+# print("Addition of 5 and 3:", add(5,3))
+# print("Subtraction of 5 and 3:", sub(5,3))
+
+find_largest_word = lambda text: max(text.split(), key=len)
+# text = input("Enter a sentence: ")
+# largest_word = find_largest_word(text)
+# print(f"The longest word in the sentence is: {largest_word}")
+
+
+
+import time
+
+text  = input("Enter a sentence: ")
+start_time = time.perf_counter()
+largest_word = find_largest_word(text)
+
+
+end_time = time.perf_counter()
+print(f"Largest word: {largest_word}")
+execution_time = end_time - start_time
+
+print(f"Execution time: {execution_time:.6f} seconds")
+
+
--- Day_6/longest.py
+++ Day_6/longest.py
@@ -5,7 +5,26 @@ def longest_word(text):
         if len(word) > len(x):
             x = word
     return x
+    # y = max(words, key=len)
+    # return y
 
-x = input("Enter a sentence: ")
-result = longest_word(x)
-print(f"The longest word in the sentence is: {result}")
+
+# x = input("Enter a sentence: ")
+# result = longest_word(x)
+# print(f"The longest word in the sentence is: {result}")
+
+
+import time
+
+text  = input("Enter a sentence: ")
+start_time = time.perf_counter() #high-resolution timer to measure execution time better than time.time()
+
+result = longest_word(text)
+print(f"Longest word: {result}")
+
+end_time = time.perf_counter()
+execution_time = end_time - start_time
+
+print(f"Execution time: {execution_time:.6f} seconds")
+
+
